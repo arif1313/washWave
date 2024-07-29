38ca66335c90aa137cea37bee305a7a3930935d5
arif1313 <arifhossen13131@gmail.com>
2024-07-29 18:14:29 +0600

    zod error formation

--- src/app/modules/Middelwares/globalError.ts
+++ src/app/modules/Middelwares/globalError.ts
@@ -1,19 +1,31 @@
+/* eslint-disable prefer-const */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import { ZodError } from 'zod';
 
-const globalErrorHandeler = (
-  error: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const statuscode = error.statusCode || 500;
-  const message = error.message || 'something went wrong';
+const globalErrorHandeler: ErrorRequestHandler = (error, req, res, next) => {
+  let statuscode = error.statusCode || 500;
+  let message = error.message || 'something went wrong';
+  type TErorSorce = {
+    path: string | number;
+    message: string;
+  }[];
+  let errorSource: TErorSorce = [
+    {
+      path: '',
+      message: 'something went wrong',
+    },
+  ];
+  if (error instanceof ZodError) {
+    statuscode = 400;
+    message = 'this is zod error';
+  }
+
   return res.status(statuscode).json({
     success: false,
     message,
-    error,
+    errorSource,
   });
 };
 export default globalErrorHandeler;
