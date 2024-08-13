235f29704379a7e56f046d948c5bfc58ebd40b5e
arif1313 <arifhossen13131@gmail.com>
2024-08-14 01:35:44 +0600

    welcome sms

--- src/app.ts
+++ src/app.ts
@@ -11,8 +11,9 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 app.use('/api', router);
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('hellow');
+  res.send('hello wellcome washWave project');
 });
 app.use(globalErrorHandeler);
 app.use(notFound);
