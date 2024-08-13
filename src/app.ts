55b3fd560045e7d0db7f735aed67a1110635b88f
arif1313 <arifhossen13131@gmail.com>
2024-08-14 01:32:29 +0600

    deployment success

--- src/app.ts
+++ src/app.ts
@@ -11,12 +11,10 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 app.use('/api', router);
-
-app.use(globalErrorHandeler);
-app.use(notFound);
-
 app.get('/', (req: Request, res: Response) => {
   res.send('hellow');
 });
+app.use(globalErrorHandeler);
+app.use(notFound);
 
 export default app;
